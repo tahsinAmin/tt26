67fe3e15851092f9ca5c636448017ca9c4c8a8cb
tahsin <amin1996bangladesh@gmail.com>
2026-02-02 02:55:14 +0600

    feat: Group Memebers details added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,28 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="min-h-svh max-w-7xl mx-auto">
+      {/* <Button>Click me</Button> */}
+
+      <h1 className="text-7xl font-bold mb-6 text-[#00456b]">Table Tennis Singles 2026 (Unofficial)</h1>
+      <section>
+        <div className="text-2xl">
+          <strong>Group A1</strong>: Mamun, Neloy, Sagar, Shagato, Mubin, B-Qualifier 1
+        </div>
+        <div className="text-2xl">
+          <strong>Group A2</strong>: Fazle, Larry, Aminul, Tahsin, Topu, B-Qualifier 2
+        </div>
+        <div className="text-2xl">
+          <strong>Group A3</strong>:  Ariful Islam, Sakib, Fazle Rabbi, Naeem, Jaber, B-Qualifier 3</div>
+        <div className="text-2xl">
+          <strong>Group A4</strong>: Nadim, Tahseen, Farhan, Saiful, Partha, B-Qualifier 4</div>
+        <div className="text-2xl">
+          <strong>Group B1</strong>: Samrat Ghosh, Shohel Rana, Shohag, Raihan, Mehedi
+        </div>
+        <div className="text-2xl">
+          <strong>Group B2</strong>: Imtiaz, Ashikur, Jaminur, Masum, Tuhin
+        </div>
+      </section>
     </div>
   );
 }
